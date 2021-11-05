2191c881f915d2fd066825e33357c7ce9f196b09
Rafa-Pinheiro <rafaela.spinheiros@gmail.com>
2021-11-06 08:50:55 +0900

    Função cadastrar feita

--- www/lib/motor.js
+++ www/lib/motor.js
@@ -1,7 +1,24 @@
 window.onload = function(){
-  const buscar = document.querySelector("#buscar");
+  const buscar = document.querySelector("#bid");
   const cad = document.querySelector("#cadastrar");
   const alt = document.querySelector("#alterar");
   const del = document.querySelector("#deletar");
+  const id = document.querySelector("#id");
+  const curso = document.querySelector("#curso");
+  const nome = document.querySelector("#nome");
+
+  cadastrar.addEventListener("click", function(){
+    let formdata = new FormData();
+    formdata.append("nome",`${nome.value}`);
+    formdata.append("curso", `${curso.value}`);
+
+    fetch('https://jussimarleal.com.br/exemplo_api/pessoa',{
+      body:formdata,
+      method: 'post',
+      mode: 'cors',
+      cache: 'default'
+    })
+      .then(altert("Cadastro efetuado com sucesso!"));
+  });
 }
 
